2566a553c59c0c06ad2d63e338928162035e14e5
MuriloDutra <murilo.dutra2000@gmail.com>
2019-11-02 00:40:38 -0200

    Continuando tutorial

---
+++ index.js
@@ -0,0 +1,7 @@
+import { addTodo } from './actions/action'
+
+function dispara(){
+    console.log('FOI CHAMADO')
+}
+
+document.getElementById('button').addEventListener('click', dispara)
--- store/store.js
+++ store/store.js
@@ -25,3 +25,16 @@ store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
 
 // Stop listening to state updates
 unsubscribe()
+
+
+/**
+ * A store armazena todo o state da aplicação, a única maneira de mudar o state interior da Store é disparar uma ação
+ * A store não é uma Classe, mas sim um objeto com alguns métodos dentro
+ * 
+ * MÉTODOS DA STORE
+ * 
+ * getState() retorna o state atual da aplicação
+ * 
+ * dispatch() dispara uma ação, tal ação do usuário possui dados que irão mudar o state da Store. O único jeito de mudar 
+ * o state da Store é usando o método dispatch()
+ */
